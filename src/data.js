870da06d9c64ae6d794b090d0d218fe085e2e19f
Domas <dominykas058@gmail.com>
2022-10-13 22:12:17 +0300

    Pirmas YouTube tutorialo projektas (su mano lengvom modifikacijom)

---
+++ src/data.js
@@ -0,0 +1,44 @@
+export default [
+  {
+    id: 1,
+    name: 'Domantas Sabonis',
+    age: 26,
+    image:
+      'https://a.espncdn.com/combiner/i?img=/i/headshots/nba/players/full/3155942.png',
+  },
+  {
+    id: 2,
+    name: 'Mindaugas Kuzminskas',
+    age: 32,
+    image:
+      'https://cdn.nba.com/headshots/nba/latest/1040x760/1627851.png',
+  },
+  {
+    id: 3,
+    name: 'Marius Grigonis',
+    age: 28,
+    image:
+      'https://www.fiba.basketball/api/img/player/coverimage/5/9604/183013/550/418',
+  },
+  {
+    id: 4,
+    name: 'Paulius Jankūnas',
+    age: 38,
+    image:
+    'https://lkl.lt/media/players/890x520/8adbd29c26be5d789398607854126b6f.png',
+  },
+  {
+    id: 5,
+    name: 'Mantas Kalnietis',
+    age: 36,
+    image:
+      'https://m.basketnews.lt/paveikslelis-360631-vbg.jpg',
+  },
+  {
+    id: 6,
+    name:'Jonas Valančiūnas',
+    age: 30,
+    image:
+    "https://cdn.nba.com/headshots/nba/latest/1040x760/202685.png",
+  },
+];
